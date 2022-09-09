c28c520ab6ab90628c334e568de2cb7e8f0b7844
LicoriceAlex <98466799+LicoriceAlex@users.noreply.github.com>
2022-09-09 22:43:26 +0500

    urls до исправлений

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('create/', views.post_create, name='post_create'),
     path('posts/<post_id>/edit/', views.post_edit, name='post_edit'),
     path('group/<slug:slug>/', views.group_posts, name='group_list'),
-    path('<str:username>/', views.profile, name='profile'),
+    path('profile/<str:username>/', views.profile, name='profile'),
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
     path('posts/<int:post_id>/comment/',
          views.add_comment,
@@ -17,12 +17,12 @@ urlpatterns = [
 
     path('follow/', views.follow_index, name='follow_index'),
     path(
-        '<str:username>/follow/',
+        'profile/<str:username>/follow/',
         views.profile_follow,
         name='profile_follow'
     ),
     path(
-        '<str:username>/unfollow/',
+        'profile/<str:username>/unfollow/',
         views.profile_unfollow,
         name='profile_unfollow'
     ),
