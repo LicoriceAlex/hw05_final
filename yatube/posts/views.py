880386570f8f4eaf7059da286c09b097a5a3327d
LicoriceAlex <98466799+LicoriceAlex@users.noreply.github.com>
2022-09-09 23:06:03 +0500

    get_or_create добавлено

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -143,10 +143,8 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
-    following = Follow.objects.filter(
-        user=request.user, author=author).exists()
-    if request.user != author and not following:
-        Follow.objects.create(user=request.user, author=author)
+    Follow.objects.get_or_create(
+        user=request.user, author=author)
     return redirect('posts:profile', username=username)
 
 
