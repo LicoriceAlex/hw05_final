1a066626f25e92ecd5de9604951a58efb885831a
alex <aleksandrsavin03@mail.ru>
2023-02-09 19:09:49 +0500

    Prepare for nginx

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -133,7 +133,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:index'
 # подключаем движок filebased.EmailBackend
